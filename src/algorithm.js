a58ed13b5fc536f656e03837c235b8bdaca769e4
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2016-05-23 10:32:55 +0100

    Minor tweak in algorithm

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,21 +1,21 @@
-import { compose, map, isArray } from "./transducers.js";
-import { curry } from "./functions.js";
+import { map, isArray } from "./transducers.js";
+import { compose, curry, identity } from "./functions.js";
 import * as dom from "./dom.js";
 
 export default DomAlgorithm;
 
-const identity = map((x) => x);
+const identityXform = map(identity);
 
 function DomAlgorithm() {
-    let transducer = identity;
+    let transducer = identityXform;
     let prototype = {};
 
-    Object.keys(dom).forEach((key) => prototype[key] = (...args) => {
+    Object.keys(dom).forEach(key => prototype[key] = (...args) => {
         transducer = compose(map(curry(dom[key], ...args)), transducer);
         return prototype;
     });
 
-    prototype.run = (item) => item instanceof Node || item === document ?
+    prototype.run = item => item instanceof Node || item === document ?
         into([], transducer, [item])[0] :
         isArray(item) ?
         into([], transducer, item) :
