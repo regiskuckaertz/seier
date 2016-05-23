1b7ac8b108222dc6163ea571633e7da2f2649404
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2016-05-23 10:32:31 +0100

    Add babel to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
     format: 'iife',
     moduleName: 'seier',
     entry: 'src/seier.js',
     dest: 'build/seier.js',
-    plugins: [ babel() ]
+    plugins: [ uglify(), babel() ]
 };
